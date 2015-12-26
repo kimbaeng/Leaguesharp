8c36c7442e981410c1d11a7124b3bacbfbcda451
kimbaeng <wndma1224@naver.com>
2015-12-26 11:04:39 +0900

    A

--- Kimbaeng Shen/Kimbaeng Shen/Program.cs
+++ Kimbaeng Shen/Kimbaeng Shen/Program.cs
@@ -277,22 +277,18 @@ namespace Kimbaeng_Shen
 
             if (FTarget != null && STarget != null)
             {
-                if (FTarget.IsValidTarget() && STarget.IsValidTarget())
+
+                if (FTarget.IsValidTarget(E.Range) && STarget.IsValidTarget(EFlash.Range))
+                {
+                    //var Endpos = ObjectManager.Player.Position.Extend(FTarget.Position, E.Range);
                     E.Cast(FTarget);
-
-                if (FTarget.HasBuffOfType(BuffType.Taunt) && ObjectManager.Player.IsDashing()
-                    && ObjectManager.Player.Distance(STarget) < 410) ObjectManager.Player.Spellbook.CastSpell(FlashSlot, STarget);
-                //if (FTarget.IsValidTarget(E.Range - 10) && STarget.IsValidTarget(EFlash.Range))
-                //{
-                //    //var Endpos = ObjectManager.Player.Position.Extend(FTarget.Position, E.Range);
-                //    E.Cast(FTarget);
-                //}
+                }
 
 
-                //if (FTarget.HasBuffOfType(BuffType.Taunt) && ObjectManager.Player.IsDashing() && ObjectManager.Player.Distance(STarget.Position) < 410)
-                //{
-                //    ObjectManager.Player.Spellbook.CastSpell(FlashSlot, STarget.Position);
-                //}
+                if (FTarget.HasBuffOfType(BuffType.Taunt) && ObjectManager.Player.IsDashing() && ObjectManager.Player.Distance(STarget.Position) < 410)
+                {
+                    ObjectManager.Player.Spellbook.CastSpell(FlashSlot, STarget.Position);
+                }
             }
 
             if (EFTarget != null)
