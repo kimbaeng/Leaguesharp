df9bde4b11455ba77852e54c4017bd7958a993b7
kimbaeng <wndma1224@naver.com>
2016-06-24 17:14:03 +0900

    del

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
